ea0f845a9a257e3e2a426f4167c31659b521f710
findawayer <findawayer@gmail.com>
2021-01-24 13:16:48 +0900

    불필요한 조건 제거

--- src/week1.merge-intervals/findawayer.js
+++ src/week1.merge-intervals/findawayer.js
@@ -1,5 +1,6 @@
-// Runtime: 84 ms, faster than 95.23% of JavaScript online submissions for Merge Intervals.
-// Memory Usage: 41 MB, less than 42.93% of JavaScript online submissions for Merge Intervals.
+// https://leetcode.com/problems/merge-intervals/
+// Runtime: 84 ms, faster than 95.30% of JavaScript online submissions for Merge Intervals.
+// Memory Usage: 39.8 MB, less than 99.08% of JavaScript online submissions for Merge Intervals.
 function solution(intervals) {
   const { length } = intervals;
 
@@ -16,7 +17,7 @@ function solution(intervals) {
     current = intervals[i];
 
     // 범위 I의 시작점이 현재 범위 안에 포함될 경우 두 범위를 병합
-    if (top[0] <= current[0] && current[0] <= top[1]) {
+    if (current[0] <= top[1]) {
       top[1] = Math.max(current[1], top[1]);
     }
     // 포함되지 않을 경우 스택을 갱신
