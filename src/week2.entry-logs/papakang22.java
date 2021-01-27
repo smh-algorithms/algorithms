b62974e6272dc472f0c6d8b73a48d593cf827369
nuhwang <nuhwang@amorepacific.com>
2021-01-27 13:18:12 +0900

    Revert "entryLog 풀이"
    This reverts commit 058f139fe0e9868fb4323b2158fe98f4d18813ac.

---
+++ src/week2.entry-logs/papakang22.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+//메모리 : 54152KB , 시간 1480ms
+public class Main {
+	public static void main(String[] args) {
+		try {
+			entryLogs();
+		}catch(Exception e) {
+			
+		}
+	}
+	
+	public static void entryLogs() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		Map<String,String> map = new HashMap<>();
+		for(int i = 0 ; i < n;i++) {
+			st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			String method = st.nextToken();
+			if(method.equals("enter")) {
+				map.put(name,name);
+			}else {
+				map.remove(name);
+			}
+		}
+		
+		Object[] mapKey = map.keySet().toArray();
+		Arrays.sort(mapKey, Collections.reverseOrder());
+		
+		for(Object name : mapKey) {
+			System.out.println((String)name);
+		}
+		
+	}
+}
