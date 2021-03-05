76b915e1e7440c3439010c8864f0648bbd63e427
Tae Sung Lim <findawayer@gmail.com>
2021-03-05 21:17:42 +0900

    product-fib-numbers 풀이 추가

---
+++ src/week7.product-of-consecutive-fib-numbers/findawayer.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5541f58a944b85ce6d00006a
+
+function productFib(product) {
+  let [a, b] = [0, 1];
+  let ab = a * b;
+  do {
+    if (product <= ab) return [a, b, ab === product];
+    [a, b] = [b, a + b];
+    ab = a * b;
+  } while (ab < Number.MAX_SAFE_INTEGER);
+}
+
+describe('product-of-consecutive-fib-numbers', () => {
+  test.each`
+    input  | output
+    ${714} | ${[21, 34, true]}
+    ${800} | ${[34, 55, false]}
+  `('returns $output from $input', ({ input, output }) => {
+    expect(solution(input)).toEqual(output);
+  });
+});
