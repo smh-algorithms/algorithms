fc6e3fcd76ac74cb71b1299b1c961198048698cb
Tae Sung Lim <findawayer@gmail.com>
2021-03-17 22:57:17 +0900

    트윅

--- src/week9.add-digits/findawayer.js
+++ src/week9.add-digits/findawayer.js
@@ -1,4 +1,9 @@
-const addDigits = num => (num ? num % 9 || 9 : num);
+// https://leetcode.com/problems/add-digits/
+
+// Runtime: 100 ms, faster than 61.39% of JavaScript online submissions for Add Digits.
+// Memory Usage: 40.2 MB, less than 53.47% of JavaScript online submissions for Add Digits.
+
+const addDigits = num => num && (num % 9 || 9);
 
 describe('add-digits', () => {
   test.each`
